c60115f7775abdcd1f5b71ee4da873db6eff516a
5j9 <5j9@users.noreply.github.com>
2019-09-06 12:37:34 +0430

    feat: define some query methods

--- mwpy/__init__.py
+++ mwpy/__init__.py
@@ -1,37 +1,103 @@
-from aiohttp import ClientSession
-from typing import Dict
-from warnings import warn
+from typing import Dict, AsyncGenerator
+from logging import warning
+
+from asks import Session
+from trio import sleep
 
 
 __version__ = '0.1.dev0'
 
 
+class APIError(RuntimeError):
+    pass
+
+
+# noinspection PyShadowingBuiltins
 class API:
 
-    def __init__(self, url: str):
+    def __init__(
+        self, url: str, maxlag: int = 5,
+        user_agent: str = f'mwpy v{__version__}'
+    ) -> None:
+        """Initialize API object.
+
+        :param url: the api's url, e.g.
+            https://en.wikipedia.org/w/api.php
+        :param maxlag: see:
+            https://www.mediawiki.org/wiki/Manual:Maxlag_parameter
+        :param user_agent: see:
+            https://www.mediawiki.org/wiki/API:Etiquette#The_User-Agent_header
+        """
         self.url = url
-        self.session = ClientSession()
+        self.session = Session(
+            connections=1, headers={'User-Agent': user_agent})
+        self.maxlag = maxlag
+        self.user_agent = user_agent
 
     async def post(self, data: dict) -> dict:
-        data.update({
+        """Post a request to MW API and return the json response.
+
+        Add format, formatversion and errorformat, maxlag and utf8.
+        Warn about warnings and raise errors as APIError.
+        """
+        data = {
+            **data,
             'format': 'json',
             'formatversion': '2',
-            'errorformat': 'plaintext'})
-        async with self.session.post(self.url, data=data) as resp:
-            json = await resp.json()
-            if 'warnings' in json:
-                warn(str(json['warnings']))
-            if 'errors' in json:
-                raise RuntimeError(json['errors'])
-            return json
+            'errorformat': 'plaintext',
+            'utf8': '',
+            'maxlag': self.maxlag}
+        resp = await self.session.post(self.url, data=data)
+        json = resp.json()
+        if 'warnings' in json:
+            warning(str(json['warnings']))
+        if 'errors' in json:
+            return await self._handle_api_errors(data, resp, json['errors'])
+        return json
 
-    async def tokens(self, types: str) -> Dict[str, str]:
-        return (await self.post({
-            'action': 'query',
-            'meta': 'tokens',
-            'type': types}))['query']['tokens']
+    async def _handle_api_errors(self, data, resp, errors):
+        for error in errors:
+            if error['code'] == 'maxlag':
+                retry_after = resp.headers['retry-after']
+                warning(f'maxlag error (retry after {retry_after} seconds)')
+                await sleep(int(retry_after))
+                return await(self.post(data))
+        raise APIError(errors)
 
-    async def login(self, name, password, domain=''):
+    async def query(self, params: dict) -> AsyncGenerator[dict, None]:
+        """Post an API query and yeild results.
+
+        Handle continuations.
+
+        https://www.mediawiki.org/wiki/API:Query
+        """
+        if 'rawcontinue' in params:
+            raise NotImplementedError(
+                'rawcontinue is implemented for query method')
+        params = {**params, 'action': 'query'}
+        while True:
+            json = await self.post(params)
+            continue_ = json.get('continue')
+            yield json
+            if continue_ is None:
+                return
+            params = {**params, **continue_}
+
+    async def tokens(self, type: str) -> Dict[str, str]:
+        """Query API for tokens. Return the json response.
+
+        https://www.mediawiki.org/wiki/API:Tokens
+        """
+        return await self.meta_query({'meta': 'tokens', 'type': type})
+
+    async def login(self, name: str, password: str, domain=None) -> None:
+        """Login using bot name and bot password.
+
+        Should only be used in combination with Special:BotPasswords;
+        use for main-account login is deprecated and may fail without warning.
+
+        https://www.mediawiki.org/wiki/API:Login
+        """
         await self.post({
             'action': 'login',
             'lgname': name,
@@ -39,7 +105,8 @@ class API:
             'lgdomain': domain,
             'lgtoken': (await self.tokens("login"))["logintoken"]})
 
-    async def close(self):
+    async def close(self) -> None:
+        """Close the current API session."""
         await self.session.close()
 
     async def __aenter__(self):
@@ -47,3 +114,100 @@ class API:
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         await self.close()
+
+    async def list_query(self, params: dict):
+        """Post a list query and yield the results.
+
+        https://www.mediawiki.org/wiki/API:Lists
+        """
+        list_ = params['list']
+        async for json in self.query(params):
+            assert json['batchcomplete'] is True  # T84977#5471790
+            for item in json['query'][list_]:
+                yield item
+
+    async def prop_query(self, params: dict):
+        """Post a prop query, handle batchcomplete, and yield the results.
+
+        https://www.mediawiki.org/wiki/API:Properties
+        """
+        prop_name = params['prop']
+        batch = {}
+        batch_get = batch.get
+        batch_clear = batch.clear
+        batch_setdefault = batch.setdefault
+        async for json in self.query(params):
+            if 'batchcomplete' in json:
+                if not batch:
+                    for props in json['query']['pages'].values():
+                        yield props[prop_name]
+                else:
+                    for page_id, props in json['query']['pages'].items():
+                        batch_props = batch_get(page_id)
+                        if batch_props is None:
+                            yield props[prop_name]
+                        else:
+                            batch_prop = batch_props[prop_name]
+                            batch_prop += props[prop_name]
+                            yield batch_prop
+                    batch_clear()
+            else:
+                for page_id, props in json['query']['pages'].items():
+                    batch_props = batch_setdefault(page_id, props)
+                    if props is not batch_props:
+                        batch_props.update(props)
+
+    async def meta_query(self, params: dict):
+        """Post a meta query and yield the results.
+
+        Note: siteinfo module requires special handling. Use self.siteinfo()
+            instead. There may be other meta queries that also require special
+            care. Use self.query() if there is no specific method for that
+            meta query.
+
+        https://www.mediawiki.org/wiki/API:Meta
+        """
+        meta = params['meta']
+        if meta == 'siteinfo':
+            raise NotImplementedError('use self.siteinfo() instead.')
+        if meta == 'filerepoinfo':
+            meta = 'repos'
+        async for json in self.query(params):
+            assert json['batchcomplete'] is True
+            return json['query'][meta]
+
+    async def siteinfo(
+        self, prop: str = None, filteriw: str = None, showalldb: str = None,
+        numberingroup: str = None, inlanguagecode: str = None,
+    ) -> dict:
+        """https://www.mediawiki.org/wiki/API:Siteinfo"""
+        async for json in self.query({
+            'meta': 'siteinfo',
+            'siprop': prop,
+            'sifilteriw': filteriw,
+            'sishowalldb': showalldb,
+            'sinumberingroup': numberingroup,
+            'siinlanguagecode': inlanguagecode,
+        }):
+            assert 'batchcomplete' in json
+            assert 'continue' not in json
+            return json['query']
+
+    async def recentchanges(
+        self, start: str = None, end: str = None, dir: str = None,
+        namespace: str = None, user: str = None, excludeuser: str = None,
+        tag: str = None, prop: str = None, show: str = None,
+        limit: int = 'max', type: str = None, toponly: bool = None,
+        title: str = None
+    ):
+        """https://www.mediawiki.org/wiki/API:RecentChanges"""
+        # Todo: somehow support rcgeneraterevisions
+        async for rc in self.list_query({
+            'list': 'recentchanges', 'rcstart': start, 'rcend': end,
+            'rcdir': dir, 'rcnamespace': namespace, 'rcuser': user,
+            'rcexcludeuser': excludeuser, 'rctag': tag, 'rcprop': prop,
+            'rcshow': show, 'rclimit': limit, 'rctype': type,
+            'rctoponly': toponly, 'rctitle': title,
+
+        }):
+            yield rc
--- test/test.py
+++ test/test.py
@@ -1,35 +1,58 @@
-from asyncio import run
-from unittest.mock import patch, call
 from unittest import main, TestCase
+from unittest.mock import patch, call
+
+from trio import run
+
 from mwpy import API
 
 
 async def fake_token_post():
-    return {'query': {'tokens': {'logintoken': None}}}
+    return {'query': {'tokens': {'logintoken': 'LOGIN_TOKEN'}}, 'batchcomplete': True}
 
 
 async def fake_login_post():
-    return
+    return {'query': {'tokens': {'logintoken': 'LOGIN_TOKEN'}}}
+
+
+async def fake_rc_post1():
+    return {'batchcomplete': True, 'continue': {'rccontinue': '20190908072938|4484663', 'continue': '-||'}, 'query': {'recentchanges': [{'type': 'log', 'timestamp': '2019-09-08T07:30:00Z'}]}}
+
+
+async def fake_rc_post2():
+    return {'batchcomplete': True, 'query': {'recentchanges': [{'type': 'categorize', 'timestamp':'2019-09-08T07:29:38Z'}]}}
+
+
+api = API('https://www.mediawiki.org/w/api.php')
 
 
 class APITest(TestCase):
 
-    @staticmethod
-    async def login_test():
-        async with API('https://www.mediawiki.org/w/api.php') as api:
-            with patch.object(
-                api, 'post',
-                side_effect=(fake_token_post(), fake_login_post()),
-            ) as post_patch:
-                await api.login('U', 'P')
-            post_patch.assert_has_calls((
-                call({'action': 'query', 'meta': 'tokens', 'type': 'login'}),
-                call({
-                    'action': 'login', 'lgname': 'U', 'lgpassword': 'P',
-                    'lgdomain': '', 'lgtoken': None})))
+    @patch.object(
+        api, 'post', side_effect=(fake_token_post(), fake_login_post()))
+    async def login_test(self, post_patch):
+        await api.login('U', 'P')
+        post_patch.assert_has_calls((
+            call({'action': 'query', 'meta': 'tokens', 'type': 'login'}),
+            call({'action': 'login', 'lgname': 'U', 'lgpassword': 'P', 'lgdomain': None, 'lgtoken': 'LOGIN_TOKEN'})))
 
     def test_login(self):
-        run(self.login_test())
+        run(self.login_test)
+
+    @patch.object(
+        api, 'post', side_effect=(fake_rc_post1(), fake_rc_post2()))
+    async def recentchanges_test(self, post_patch):
+        self.assertEqual(
+            [rc async for rc in api.recentchanges(limit=1, prop='timestamp')],
+            [
+                {'type': 'log', 'timestamp': '2019-09-08T07:30:00Z'},
+                {'type': 'categorize', 'timestamp': '2019-09-08T07:29:38Z'}])
+        post1_call_data = {'list': 'recentchanges', 'rcstart': None, 'rcend': None, 'rcdir': None, 'rcnamespace': None, 'rcuser': None, 'rcexcludeuser': None, 'rctag': None, 'rcprop': 'timestamp', 'rcshow': None, 'rclimit': 1, 'rctype': None, 'rctoponly': None, 'rctitle': None, 'action': 'query'}
+        post2_call_data = {**post1_call_data, 'rccontinue': '20190908072938|4484663', 'continue': '-||'}
+        post_patch.assert_has_calls(
+            [call(post1_call_data), call(post2_call_data)])
+
+    def test_recentchanges(self):
+        run(self.recentchanges_test)
 
 
 if __name__ == '__main__':
